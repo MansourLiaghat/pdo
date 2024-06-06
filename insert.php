96281e61b267fbb6f89e35f72e03728a7140b3a6
Mansour Liaghat <mansourliaghat@yahoo.com>
2024-06-06 09:18:03 +0330

    feat(insert): create user and save to database

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once 'connect.php';
+
+
+$sql = "INSERT INTO user (id, fullName, age, city) VALUES (?,?,?,?)";
+
+$stmt = $db->prepare($sql);
+
+$users = [
+    [1, 'aren', 2, 'kish'],
+    [2, 'mahsa', 31, 'tehran'],
+    [3, 'mansour', 34, 'golpayegan']
+];
+
+$db->beginTransaction();
+foreach ($users as $user) {
+    $stmt->execute($user);
+    if ($user[1][2] == 31) {
+        exit();
+    }
+}
+$db->commit();
